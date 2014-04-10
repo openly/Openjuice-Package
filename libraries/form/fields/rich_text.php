b270ce5c3148111b0e34801148e977a31ab13ff9
Navya H.K <navya.k@openly.co>
2014-04-10 19:13:34 +0530

    changes

--- libraries/form/fields/rich_text.php
+++ libraries/form/fields/rich_text.php
@@ -1,16 +1,20 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 
-class RichTextField extends OJField{
-	public function initialize(){
-		parent::initialize();
-		$this->fieldAttrs['class'] .= ' ccm-advanced-editor';
-		$form = Loader::helper('form');
-		$this->field = $form->textarea($this->getDisplayFieldName(),$this->default,$this->fieldAttrs);
-		if(!$_GLOBALS['mce_conf']){
-			Loader::element('editor_init');
-			Loader::element('editor_config', array('editor_mode' => 'ADVANCED'));
-			$_GLOBALS['mce_conf'] = 1;
-		}
-	}
+class RichTextField extends OJField
+{
+    public function initialize()
+    {
+        parent::initialize();
+        $this->fieldAttrs['class'] .= ' ccm-advanced-editor';
+
+        $form = Loader::helper('form');
+        $this->field   = $template = file_get_contents(dirname(__FILE__) . '/../../../assets/templates/editor_controls.html');
+        $this->field   .= $form->textarea($this->getDisplayFieldName(), $this->default, $this->fieldAttrs);
+        if (!$_GLOBALS['mce_conf']) {
+            Loader::element('editor_init');
+            Loader::element('editor_config', array('editor_mode' => 'ADVANCED'));
+            $_GLOBALS['mce_conf'] = 1;
+        }
+    }
 }
--- libraries/form/fields/text_area.php
+++ libraries/form/fields/text_area.php
@@ -1,10 +1,13 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 
-class TextAreaField extends OJField{
-	public function initialize(){
-		parent::initialize();
-		$form = Loader::helper('form');
-		$this->field = $form->textarea($this->getDisplayFieldName(),$this->default,$this->fieldAttrs);
-	}
+class TextAreaField extends OJField
+{
+    public function initialize()
+    {
+        parent::initialize();
+        $form = Loader::helper('form');
+        $this->fieldAttrs = array('rows'=>"10","cols"=>"50","style"=>"width:auto;");
+        $this->field = $form->textarea($this->getDisplayFieldName(), $this->default, $this->fieldAttrs);
+    }
 }
